2cc62cbebbadda5fbfac5fda3523e3d606f0657b
Satyam Singh <satyamsingh877a@gmail.com>
2026-06-10 22:28:16 +0530

    Create RepeatedSubstring.cpp

---
+++ Week 2/Day 3/RepeatedSubstring.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) {
+        int n = s.length();
+
+        for (int len = 1; len <= n / 2; len++) {
+            if (n % len == 0) {
+                string pattern = s.substr(0, len);
+                string temp = "";
+
+                int times = n / len;
+                for (int i = 0; i < times; i++) {
+                    temp += pattern;
+                }
+
+                if (temp == s) return true;
+            }
+        }
+
+        return false;
+    }
+};
