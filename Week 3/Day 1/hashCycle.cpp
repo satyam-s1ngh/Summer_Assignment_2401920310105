fb099bb7d54b15f7483a2c827c409d97b31013ad
Satyam Singh <satyamsingh877a@gmail.com>
2026-06-16 21:55:35 +0530

    Update hashCycle.cpp

--- Week 3/Day 1/hashCycle.cpp
+++ Week 3/Day 1/hashCycle.cpp
@@ -1,22 +1,18 @@
-#include<bits/stdc++.h>
-
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode(int x) : val(x), next(nullptr) {}
-};
-
-
-class Solution{
+class Solution {
 public:
-    bool hasCycle(ListNode* head){
-        ListNode* slow=head;
-        ListNode* fast=head;
-        while(fast&&fast->next){
-            slow=slow->next;
-            fast=fast->next->next;
-            if(slow==fast)return true;
+    bool hasCycle(ListNode* head) {
+        if (head == NULL || head->next == NULL) {
+            return false;
         }
-        return false;
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+        while (fast != slow) {
+            if (fast->next == NULL || fast->next->next == NULL) {
+                return false;
+            }
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return true;
     }
 };
