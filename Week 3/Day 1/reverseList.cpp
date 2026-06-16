dd0f5a89ca2a1cd522bf590a9c7f8d6f3043e39d
Satyam Singh <satyamsingh877a@gmail.com>
2026-06-16 21:57:13 +0530

    Refactor reverseList.cpp by cleaning up includes
    Removed unnecessary include and ListNode struct definition.

--- Week 3/Day 1/reverseList.cpp
+++ Week 3/Day 1/reverseList.cpp
@@ -1,11 +1,3 @@
-#include<bits/stdc++.h>
-
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode(int x) : val(x), next(nullptr) {}
-};
-
 class Solution{
 public:
     ListNode* reverseList(ListNode* head){
