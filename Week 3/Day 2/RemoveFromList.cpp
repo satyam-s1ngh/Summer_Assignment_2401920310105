45c93b638d4b5c42ee2449c89da5c4485da4179e
Satyam Singh <satyamsingh877a@gmail.com>
2026-06-16 22:02:03 +0530

    Update RemoveFromList.cpp

--- Week 3/Day 2/RemoveFromList.cpp
+++ Week 3/Day 2/RemoveFromList.cpp
@@ -1,39 +1,3 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-class ListNode{
-    public:
-        int val;
-        ListNode* next;
-        ListNode(int d,ListNode* n){
-            val=d;
-            next=n;
-        }
-        ListNode(int d){
-            val=d;
-            next=nullptr;
-        }
-    };
-
-ListNode* createLL(vector<int> arr){
-    ListNode* head= new ListNode(arr[0]);
-    ListNode* m=head;
-    for(int i=1;i<arr.size();i++){
-        ListNode* temp=new ListNode(arr[i]);
-        m->next=temp;    
-        m=m->next;
-    }
-    return head;
-}
-
-void printLL(ListNode* head){
-    ListNode* temp=head;
-    while(temp!=nullptr){
-        cout << temp->val << " ";
-        temp=temp->next;
-    }
-}
-
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
@@ -61,12 +25,3 @@ public:
         return head;
     }
 };
-
-int main(){
-    vector<int> v={1,2,3,4,5};
-    ListNode* head=createLL(v);
-    Solution ob;
-    head=ob.removeNthFromEnd(head,2);
-    printLL(head);
-    return 0;
-}
