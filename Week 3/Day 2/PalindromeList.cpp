fac94fbf2f85e8b5935bc6141ec9716789c38ea1
Satyam Singh <satyamsingh877a@gmail.com>
2026-06-16 22:03:39 +0530

    Refactor PalindromeList.cpp by removing main function
    Removed unnecessary includes and main function from PalindromeList.cpp.

--- Week 3/Day 2/PalindromeList.cpp
+++ Week 3/Day 2/PalindromeList.cpp
@@ -1,39 +1,3 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-class ListNode{
-    public:
-        int val;
-        ListNode* next;
-        ListNode(int d,ListNode* n){
-            val=d;
-            next=n;
-        }
-        ListNode(int d){
-            val=d;
-            next=nullptr;
-        }
-    };
-
-ListNode* createLL(vector<int> arr){
-    ListNode* head= new ListNode(arr[0]);
-    ListNode* m=head;
-    for(int i=1;i<arr.size();i++){
-        ListNode* temp=new ListNode(arr[i]);
-        m->next=temp;    
-        m=m->next;
-    }
-    return head;
-}
-
-void printLL(ListNode* head){
-    ListNode* temp=head;
-    while(temp!=nullptr){
-        cout << temp->val << " ";
-        temp=temp->next;
-    }
-}
-
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
@@ -52,11 +16,3 @@ public:
         return true;
     }
 };
-
-int main(){
-    vector<int> v={1,2,2,1};
-    ListNode* head=createLL(v);
-    Solution ob;
-    cout<<ob.isPalindrome(head);
-    return 0;
-}
