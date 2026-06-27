0b1f0edc4e68f7b1adaf1030a72a16705e1a3f3e
Satyam Singh <satyamsingh877a@gmail.com>
2026-06-27 23:49:41 +0530

    Create searchinBST.cpp

---
+++ Week 4/Day 3/searchinBST.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        while (root) {
+            if (root->val == val)
+                return root;
+            if (val < root->val)
+                root = root->left;
+            else
+                root = root->right;
+        }
+        return nullptr;
+    }
+};
